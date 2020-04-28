c7d4bdefaab59a8b9bc0fc0fedc234df55b77f4a
Roman Hoffmann <hoffmann.romane@gmail.com>
2020-04-28 12:41:48 -0600

    fully operational express,mongo,mongoose skeleton api

---
+++ routes/routes.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const router = express.Router();
+
+const newSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now}
+});
+
+//create model for Schema, determine which collection to store it in
+const schemaModel = mongoose.model('collectionName', newSchema);
+
+
+
+router.get('/', async (req,res) => {
+        //find all documents for class, can add query filters/regular expressions
+        const data = await schemaModel.find().sort('name');
+        res.send(data);
+    
+    res.send(genres);
+    });
+
+router.get('/:id', async (req, res) => {
+        const data = await schemaModel.findById(req.params.id);
+        //404 Not Found
+        if (!data) return res.status(404).send('Not Found');
+        res.send(data);
+     });
+
+router.post('/', async (req, res) => {
+    const {error} = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    let genre = new schemaModel( {
+        name: req.body.name
+    });
+    genre = await genre.save();
+    res.send(genre);
+});
+
+router.put('/:id', async (req,res) => {
+    //validate, if bad entry return 400
+    const {error} = validateGenre(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    
+    const data = await schemaModel.findByIdAndUpdate(req.params.id, {name: req.body.name}, {
+        new: true
+    });
+
+    //look up course, if doesnt exist return 404
+    if (!data) return res.status(404).send('Not Found');
+   
+    res.send(data);
+});
+
+router.delete('/:id', async (req,res) => {
+    const data = newSchema.findByIdAndRemove(req.params.id);
+
+    if (!data) return res.status(404).send('Not Found');
+    //delete
+
+    res.send(data);
+})
+
+function validateGenre(genre) {
+    const schema = {
+      name: Joi.string().min(3).required()
+    };
+  
+    return Joi.validate(genre, schema);
+  }
+
+module.exports = router;
+
+
+//there is also a third update method that combines both, findbyidandupdate
+
+//delete from database
+//can also use deleteOne or deleteMany, not sure what the differences are
+async function removeData(id){
+    const result = await schemaModel.findByIdAndRemove(id);
+    console.log(result);
+}
+
+//updateMany('5ea63bbfba46fc47142599b0');
+
+//VALIDATION: should use both Joi (for client-side) and mongoose (for db-side)
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const mongoose = require ('mongoose');
+const express = require ('express');
+const testapi = require ('./routes/routes');
+const app = express();
 
 //store MongoDB cluster connection string, also set which database to use
 const dbConnectionUrl = "mongodb+srv://rhoffmann:meme6149TOO@cluster0-ah1ec.mongodb.net/databaseName?retryWrites=true&w=majority";
@@ -10,62 +13,9 @@ mongoose.connect(dbConnectionUrl,  { useNewUrlParser: true })
     .catch( err => console.error('failed to enter', err))
 
 //define new Schema Class
-const newSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    tags: [String],
-    date: {type: Date, default: Date.now}
-});
 
-//create model for Schema, determine which collection to store it in
-const schemaModel = mongoose.model('collectionName', newSchema);
+app.use(express.json());
+app.use('/api/testapi', testapi);
 
-async function createTest(){
-const instancename = new schemaModel({
-    name: 'myname2',
-    author: 'myauthor2',
-    tags: ['tag1', 'tag2']
-})
-
-const result = await instancename.save();
-console.log(result);
-}
-
-async function getData(){
-    //find all documents for class, can add query filters/regular expressions
-    const data = await schemaModel.find();
-    console.log(data);
-}
-
-//for updating by id
-async function updateData(id){
-    const data = await schemaModel.findById(id);
-    if (!data) return;
-
-    data.name = 'updatedName';
-
-    const result = await data.save();
-    console.log(result);
-
-}
-
-//for updating documents directly in database
-//first argument is query/filter object, second argument is update object (needs mongo update operator)
-async function updateMany(id){
-    const data = await schemaModel.update({_id: id}, {
-        $set : {
-            name: 'updatedName2'
-        }
-    });
-    console.log(data);
-}
-//there is also a third update method that combines both, findbyidandupdate
-
-//delete from database
-//can also use deleteOne or deleteMany, not sure what the differences are
-async function removeData(id){
-    const result = await schemaModel.findByIdAndRemove(id);
-    console.log(result);
-}
-
-//updateMany('5ea63bbfba46fc47142599b0');
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
